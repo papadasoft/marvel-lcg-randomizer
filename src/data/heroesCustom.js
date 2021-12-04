53cd0bc59994f95faa4554ee8d58f9de9492949c
Luis Felipe <papadasoft@gmail.com>
2021-12-04 18:00:00 +0100

    Actualizado con el resto de contenido fanmade Marvel.

--- src/data/heroesCustom.js
+++ src/data/heroesCustom.js
@@ -1,11 +1,52 @@
 export const heroescustom = [
     {
+        hero: 'Agente Coulson by DesignHacker',
+        heroImg: 'images/heroesCustom/AgenteCoulsonByDesingHacker.jpg',
+        alterEgo: 'Phil Coulson',
+        alterEgoImg: 'images/heroesCustom/PhilCoulsonByDesingHacker.jpg',
+        pack: 'Agente Coulson by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1EeNZtpQhDqUoBrDZHCYbYdzw2DoQzVyS'
+    },
+	{
+        hero: 'Angela by Juri Krasko',
+        heroImg: 'images/heroesCustom/AngelaByJuriKrasko.jpg',
+        alterEgo: 'Aldrif Odinsdottir',
+        alterEgoImg: 'images/heroesCustom/AldrifByJuriKrasko.jpg',
+        pack: 'Angela by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/1pI-SelLu0Owth6prqyJ2emcch8bI1n5K'
+    },
+	{
         hero: 'Antorcha Humana by LuisFe',
         heroImg: 'images/heroesCustom/AntorchaHumanaByLuisFe.jpg',
         alterEgo: 'Johnny Storm',
         alterEgoImg: 'images/heroesCustom/JohnnyStormByLuisFe.jpg',
         pack: 'Antorcha Humana by LuisFe',
 		url: 'https://drive.google.com/drive/folders/1mGXsvvbdJ-VXxfLyZmXOgO2ELRklntvF'
+    },
+	{
+        hero: 'Blade by Juri Krasko',
+        heroImg: 'images/heroesCustom/BladeByJuriKrasko.jpg',
+        alterEgo: 'Eric Brooks',
+        alterEgoImg: 'images/heroesCustom/EricBrooksByJuriKrasko.jpg',
+        pack: 'Blade by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/15wDPr8dbPHHYKg_Rw01QvlEaTgPyqUxG'
+    },
+	{
+        hero: 'Caballero Luna by DesignHacker',
+        heroImg: 'images/heroesCustom/CaballeroLunaByDesingHacker.jpg',
+        alterEgo: 'Marc Spector',
+        alterEgoImg: 'images/heroesCustom/MarcSpectorByDesingHacker.jpg',
+        pack: 'Caballero Luna by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1EeNZtpQhDqUoBrDZHCYbYdzw2DoQzVyS',
+		numberOfAspects: 4
+    },
+	{
+        hero: 'Caballero Negro by Juri Krasko',
+        heroImg: 'images/heroesCustom/CaballeroNegroByJuriKrasko.jpg',
+        alterEgo: 'Dane Whitman',
+        alterEgoImg: 'images/heroesCustom/DaneWhitmanByJuriKrasko.jpg',
+        pack: 'Caballero Negro by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/1FGQ3ia-TadPQYXHbT-RiO2e61dxFnWbV'
     },
 	{
         hero: 'Cable by Doko',
@@ -16,12 +57,29 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/1mGXsvvbdJ-VXxfLyZmXOgO2ELRklntvF'
     },
 	{
+        hero: 'Capa y Puñal by DesignHacker',
+        heroImg: 'images/heroesCustom/CapaYPunalByDesingHacker.jpg',
+        alterEgo: 'Tyrone Johnson + Tandy Bowen',
+        alterEgoImg: 'images/heroesCustom/TyroneTandyByDesingHacker.jpg',
+        pack: 'Capa y Puñal by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1EeNZtpQhDqUoBrDZHCYbYdzw2DoQzVyS',
+		numberOfAspects: 2
+    },
+	{
         hero: 'Capitana Carter (What if) by LuisFe',
         heroImg: 'images/heroesCustom/CapitanaCarterByLuisFe.jpg',
         alterEgo: 'Peggy Carter',
         alterEgoImg: 'images/heroesCustom/PeggyCarterByLuisFe.jpg',
         pack: 'Capitana Carter (What if) by LuisFe',
 		url: 'https://drive.google.com/drive/folders/1IbzedjQ40m0u9Y_2oP38YQu6ABJKI_Zq'
+    },
+	{
+        hero: 'Ciclope by P. H. Comassetto',
+        heroImg: 'images/heroesCustom/CiclopeByPHComassetto.jpg',
+        alterEgo: 'Scott Summers',
+        alterEgoImg: 'images/heroesCustom/ScottSummersByPHComassetto.jpg',
+        pack: 'Ciclope by P. H. Comassetto',
+		url: 'https://drive.google.com/drive/folders/1re5nsKPH5sfMOvPxVLreyo26raiPiy4b'
     },
 	{
         hero: 'Ciclope (5 Fenix) by Caurel-Fran',
@@ -40,12 +98,44 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/1FRmPudEEW1JBV9gLohhZ3EXi4VihTHHK'
     },
 	{
+        hero: 'Crystal by BubbaEA',
+        heroImg: 'images/heroesCustom/CrystalByBubbaEA.jpg',
+        alterEgo: 'Crystalia Amaquelin',
+        alterEgoImg: 'images/heroesCustom/CrystaliaAmaquelinByBubbaEA.jpg',
+        pack: 'Crystal by BubbaEA',
+		url: 'https://drive.google.com/drive/folders/12dK8jJSDZC8IMSrFvxj_5j7nFBPDsJeD'
+    },
+	{
+        hero: 'Daredevil by Andy Norton',
+        heroImg: 'images/heroesCustom/DaredevilByAndyNorton.jpg',
+        alterEgo: 'Matt Murdock',
+        alterEgoImg: 'images/heroesCustom/MattMurdockByAndyNorton.jpg',
+        pack: 'Daredevil by Andy Norton',
+		url: 'https://drive.google.com/drive/folders/1L3qNU8DP3iUG0Y8Pttjx3JCS2O2ZcEAF'
+    },
+	{
+        hero: 'Daredevil by Juri Krasko',
+        heroImg: 'images/heroesCustom/DaredevilByJuriKrasko.jpg',
+        alterEgo: 'Matt Murdock',
+        alterEgoImg: 'images/heroesCustom/MattMurdockByJuriKrasko.jpg',
+        pack: 'Daredevil by Juri Krasko',
+		url: 'https://drive.google.com/drive/u/0/folders/169jmWgEZl3xMqv72KC0ZEuY8fobSIZHp'
+    },
+	{
         hero: 'Daredevil by LuisFe',
         heroImg: 'images/heroesCustom/DaredevilByLuisFe.jpg',
         alterEgo: 'Matthew Murdock',
         alterEgoImg: 'images/heroesCustom/MatthewMurdockByLuisFe.jpg',
         pack: 'Daredevil by LuisFe',
 		url: 'https://drive.google.com/drive/folders/1_j36tj3ISLCQR68S_F4KnJVzzQYBRbdQ'
+    },
+	{
+        hero: 'Deadpool by DesignHacker',
+        heroImg: 'images/heroesCustom/DeadpoolByDesingHacker.jpg',
+        alterEgo: 'Wade Wilson',
+        alterEgoImg: 'images/heroesCustom/WadeWilsonByDesingHacker.jpg',
+        pack: 'Deadpool by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1m3Erb9eILdPWfGIOndQEbMIXstJfhNRN'
     },
 	{
         hero: 'Dominó by Caurel-Fran',
@@ -62,6 +152,14 @@ export const heroescustom = [
         alterEgoImg: 'images/heroesCustom/ElektraNatchiosByLuisFe.jpg',
         pack: 'Elektra by LuisFe',
 		url: 'https://drive.google.com/drive/folders/11VwrnFr9YG2vVTN4TCEAbidjGl4hRZ-K'
+    },
+	{
+        hero: 'Estela Plateada by DesignHacker',
+        heroImg: 'images/heroesCustom/EstelaPlateadaByDesingHacker.jpg',
+        alterEgo: 'Norrin Radd',
+        alterEgoImg: 'images/heroesCustom/NorrinRaddByDesingHacker.jpg',
+        pack: 'Estela Plateada by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1mP8epGnpnvcxeRMYb6iV8o_J-han7GTH'
     },
 	{
         hero: 'Fénix by Doko',
@@ -96,6 +194,14 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/1GWt5yC6sk2kqYC1lsfq79yWdEZ3S1IDl'
     },
 	{
+        hero: 'Halcón by Justatuna',
+        heroImg: 'images/heroesCustom/HalconByJustatuna.jpg',
+        alterEgo: 'Sam Wilson',
+        alterEgoImg: 'images/heroesCustom/SamWilsonByJustatuna.jpg',
+        pack: 'Halcón by Justatuna',
+		url: 'https://drive.google.com/drive/folders/129N_l71wkLlZpvnyQH9VAKFDXEM_UNdg'
+    },
+	{
         hero: 'Hombre Múltiple by Caurel-Fran',
         heroImg: 'images/heroesCustom/HombreMultipleByCaurelFran.jpg',
         alterEgo: 'Jamie Madrox',
@@ -104,12 +210,60 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/15RAfVyH8jelk41FMU10bpfSzvY9HsMyD'
     },
 	{
+        hero: 'Jessica Jones by Juri Krasko',
+        heroImg: 'images/heroesCustom/JessicaJonesByJuriKrasko.jpg',
+        alterEgo: 'Jessica Jones',
+        alterEgoImg: 'images/heroesCustom/JessicaJonesAEByJuriKrasko.jpg',
+        pack: 'Jessica Jones by Juri Krasko',
+		url: 'https://drive.google.com/drive/u/0/folders/1EAyUVlAbfW7LyVEuwTcIr_-UJfpGlGQn'
+    },
+	{
+        hero: 'Karnak by 1-2 Punchboard',
+        heroImg: 'images/heroesCustom/KarnakBy12Punchboard.jpg',
+        alterEgo: 'Karnak Mander-Azur',
+        alterEgoImg: 'images/heroesCustom/KarnakManderAzurBy12Punchboard.jpg',
+        pack: 'Karnak by 1-2 Punchboard',
+		url: 'https://drive.google.com/drive/u/0/folders/1EAyUVlAbfW7LyVEuwTcIr_-UJfpGlGQn'
+    },
+	{
         hero: 'La Cosa by LuisFe',
         heroImg: 'images/heroesCustom/LaCosaByLuisFe.jpg',
         alterEgo: 'Ben Grimm',
         alterEgoImg: 'images/heroesCustom/BenGrimmByLuisFe.jpg',
         pack: 'La Cosa by LuisFe',
 		url: 'https://drive.google.com/drive/folders/1ihYyT2TIplhlcQjMu2VOuWvoNvGsVGIU'
+    },
+	{
+        hero: 'Lobezno by Juri Krasko',
+        heroImg: 'images/heroesCustom/LobeznoByJuriKrasko.jpg',
+        alterEgo: 'Logan',
+        alterEgoImg: 'images/heroesCustom/LoganByJuriKrasko.jpg',
+        pack: 'Lobezno by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/1ns7C1yOfKArOPmnFY9KOgvKVo8HfoUeo'
+    },
+	{
+        hero: 'Maquina de guerra by DesignHacker',
+        heroImg: 'images/heroesCustom/MaquinaGuerraByDesignHacker.jpg',
+        alterEgo: 'James Rhodes',
+        alterEgoImg: 'images/heroesCustom/JamesRhodesByDesignHacker.jpg',
+        pack: 'Maquina de guerra by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1JWG8hKw6-fPYY6IbRXhmgTfH1h-MNiPK'
+    },
+	{
+        hero: 'Directora Maria Hill by Rainy',
+        heroImg: 'images/heroesCustom/DirectoraMariaHillByRaini.jpg',
+        alterEgo: 'Maria Hill',
+        alterEgoImg: 'images/heroesCustom/MariaHillByRaini.jpg',
+        pack: 'Directora Maria Hill by Rainy',
+		url: 'https://drive.google.com/drive/folders/1hXN2J2a8JHxwjk9RyfyeHf-ReiuRulLl'
+    },
+	{
+        hero: 'Motorista Fantasma by Juri Krasko',
+        heroImg: 'images/heroesCustom/MotoristaFantasmaByJuriKrasko.jpg',
+        alterEgo: 'Johnny Blaze',
+        alterEgoImg: 'images/heroesCustom/JhonnyBlazeByJuriKrasko.jpg',
+        pack: 'Motorista Fantasma by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/1ui69c6MElYoxi9NJjmxYlU1L2xIjp1qA'
     },
 	{
         hero: 'Mr. Fantástico by LuisFe',
@@ -128,12 +282,36 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/10_9c4Zv1rYn7Kb0WUKsDBgkXlZ07O_mX'
     },
 	{
+        hero: 'Namor by Juri Krasko',
+        heroImg: 'images/heroesCustom/NamorByJuriKrasko.jpg',
+        alterEgo: 'Namor McKenzie',
+        alterEgoImg: 'images/heroesCustom/NamorMcKenzieByJuriKrasko.jpg',
+        pack: 'Namor by Juri Krasko',
+		url: 'https://drive.google.com/drive/folders/1KRZ0NUI3eeTy9NH1mw62fP8R6bVePJej'
+    },
+	{
         hero: 'Nova By Doko',
         heroImg: 'images/heroesCustom/NovaByDoko.jpg',
         alterEgo: 'Richard Rider',
         alterEgoImg: 'images/heroesCustom/RichardRiderByDoko.jpg',
         pack: 'Nova by Doko',
 		url: 'https://drive.google.com/drive/folders/1cNBg2Hrxld--dQ9-m4pPqe_ltZ0qcl5S'
+    },
+	{
+        hero: 'Pájaro Burlón By Picollo',
+        heroImg: 'images/heroesCustom/PajaroBurlonByPicollo.jpg',
+        alterEgo: "Bobbi Morse",
+        alterEgoImg: 'images/heroesCustom/BobbiMorseByPicollo.jpg',
+        pack: 'Pájaro Burlón By Picollo',
+		url: 'https://drive.google.com/drive/u/0/folders/1QGZpI7Jj2PkCn4Z0a1OekYSrO91Vxekf'
+    },
+	{
+        hero: 'Pantera Negra Shuri By Picollo',
+        heroImg: 'images/heroesCustom/PanteraNegraByPicollo.jpg',
+        alterEgo: "Shuri",
+        alterEgoImg: 'images/heroesCustom/ShuriByPicollo.jpg',
+        pack: 'Pantera Negra Shuri By Picollo',
+		url: 'https://drive.google.com/drive/folders/1WIdxD0DIrYrJRPZuD785uEmft9QudhMu'
     },
 	{
         hero: 'Pícara By Doko',
@@ -144,12 +322,44 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/1AxS8MPaBq4oEdoYPyeg2NhnVA0s8-Ix8'
     },
 	{
+        hero: 'Puño de hierro by P. H. Comassetto',
+        heroImg: 'images/heroesCustom/PunoDeHierroByPHComassetto.jpg',
+        alterEgo: 'Danny Rand',
+        alterEgoImg: 'images/heroesCustom/DannyRandByPHComassetto.jpg',
+        pack: 'Puño de hierro by P. H. Comassetto',
+		url: 'https://drive.google.com/drive/folders/1KuW9xJ3h5n1Hv6ZpUCkhJ5n_QL45UE9K'
+    },
+	{
+        hero: 'Quasar by LunarBuscuit',
+        heroImg: 'images/heroesCustom/QuasarByLunarBiscuit.jpg',
+        alterEgo: 'Phylla Vell',
+        alterEgoImg: 'images/heroesCustom/PhylaVellByLunarBiscuit.jpg',
+        pack: 'Quasar by LunarBuscuit',
+		url: 'https://drive.google.com/drive/folders/1KugVD1dx4qOsu6SY3CF1c7BWCFMpBBOS'
+    },
+	{
+        hero: 'Rayo Negro by 1-2 Punchboard',
+        heroImg: 'images/heroesCustom/RayoNegroBy12Punchboard.jpg',
+        alterEgo: 'Blackagar Boltagon',
+        alterEgoImg: 'images/heroesCustom/BlackagarBoltagonBy12Punchboard.jpg',
+        pack: 'Rayo Negro by 1-2 Punchboard',
+		url: 'https://drive.google.com/drive/folders/1hoH2NqjTC2XJj7jfqgOOA87x32G03Qcm'
+    },
+	{
         hero: 'Rescate by Caurel-Fran',
         heroImg: 'images/heroesCustom/RescateByCaurelFran.jpg',
         alterEgo: "Virginia 'Pepper' Potts",
         alterEgoImg: 'images/heroesCustom/PepperPottsByCaurelFran.jpg',
         pack: 'Rescate by Caurel-Fran',
 		url: 'https://drive.google.com/drive/folders/1AxS8MPaBq4oEdoYPyeg2NhnVA0s8-Ix8'
+    },
+	{
+        hero: 'Rondador Nocturno by Dawnbreak',
+        heroImg: 'images/heroesCustom/RondadorNocturnoByDawnbreak.jpg',
+        alterEgo: "Kurt Wagner",
+        alterEgoImg: 'images/heroesCustom/KurtWagnerByDawnbreak.jpg',
+        pack: 'Rondador Nocturno by Dawnbreak',
+		url: 'https://drive.google.com/drive/folders/1OSgk-SRfShUj0SNnpLh1ftrNJDLalGct'
     },
 	{
         hero: 'S.H.I.E.L.D. by Juncal',
@@ -160,12 +370,20 @@ export const heroescustom = [
 		url: 'https://drive.google.com/drive/folders/1rPhgQuybmWyq619yn1059R2SPBiqVUpM'
     },
 	{
-        hero: 'Star Lord (What if) by LuisFe',
-        heroImg: 'images/heroesCustom/StarlordByLuisFe.jpg',
-        alterEgo: "T'Challa",
-        alterEgoImg: 'images/heroesCustom/TChallaByLuisFe.jpg',
-        pack: 'Star Lord (What if) by LuisFe',
-		url: 'https://drive.google.com/drive/folders/1WeBvOnLI0Z03aHvtJydMRfEPvEIGWLsI'
+        hero: 'Soldado de Invierno by Rainy',
+        heroImg: 'images/heroesCustom/SoldadoDeInviernoByRaini.jpg',
+        alterEgo: 'Bucky Barnes',
+        alterEgoImg: 'images/heroesCustom/BuckyBarnesByRaini.jpg',
+        pack: 'Soldado de Invierno by Rainy',
+		url: 'https://drive.google.com/drive/folders/1qC3hjmXv-K47c9Z2KLm5wuhJT_JX3byQ'
+    },
+	{
+        hero: 'Spider Gwen by Picollo',
+        heroImg: 'images/heroesCustom/SpiderGwenByPicollo.jpg',
+        alterEgo: 'Gwen Stacy',
+        alterEgoImg: 'images/heroesCustom/GwenStacyByPicollo.jpg',
+        pack: 'Spider Gwen by Picollo',
+		url: 'https://drive.google.com/drive/folders/1IaeP5L4vVe683pp_7GCBzGdmEjfkDW7G'
     },
 	{
         hero: 'Spider-Ham by OceanoDeJuegos',
@@ -174,6 +392,38 @@ export const heroescustom = [
         alterEgoImg: 'images/heroesCustom/PeterPorkerByOceano.jpg',
         pack: 'Spider-Ham by OceanoDeJuegos',
 		url: 'https://drive.google.com/folderview?id=1vnbqUpoLh1tWWtntUIOdwmibHWV6A8uX'
+    },
+	{
+        hero: 'Spider-man Miles Morales by Nugget',
+        heroImg: 'images/heroesCustom/SpidemanByNugget.jpg',
+        alterEgo: "Miles Morales",
+        alterEgoImg: 'images/heroesCustom/MilesMoralesByNugget.jpg',
+        pack: 'Spider-man Miles Morales by Nugget',
+		url: 'https://drive.google.com/folderview?id=1CJoONYN0N4F6grUewZq4ttFR-E2-r7wr'
+    },
+	{
+        hero: 'Spider-man Noir by Nugget',
+        heroImg: 'images/heroesCustom/SpidemanNoirByNugget.jpg',
+        alterEgo: "Peter Parker Noir",
+        alterEgoImg: 'images/heroesCustom/PeterParkerNoirByNugget.jpg',
+        pack: 'Spider-man Noir by Nugget',
+		url: 'https://drive.google.com/drive/u/0/folders/1hId5WF0998fBbuE7UammzBr9U2sAXGQQ'
+    },
+	{
+        hero: 'Star Lord (What if) by LuisFe',
+        heroImg: 'images/heroesCustom/StarlordByLuisFe.jpg',
+        alterEgo: "T'Challa",
+        alterEgoImg: 'images/heroesCustom/TChallaByLuisFe.jpg',
+        pack: 'Star Lord (What if) by LuisFe',
+		url: 'https://drive.google.com/drive/folders/1WeBvOnLI0Z03aHvtJydMRfEPvEIGWLsI'
+    },
+	{
+        hero: 'Thanos by BubbaEA',
+        heroImg: 'images/heroesCustom/ThanosByBubbaEA.jpg',
+        alterEgo: 'Thanos de Titán',
+        alterEgoImg: 'images/heroesCustom/ThanosDeTitanByBubbaEA.jpg',
+        pack: 'Thanos by BubbaEA',
+		url: 'https://drive.google.com/drive/folders/1d_diasPaYhePLMidChh5M2F0EfQYl7HN'
     },
 	{
         hero: 'Thor (Jane Foster) by Samu_el',
@@ -190,6 +440,30 @@ export const heroescustom = [
         alterEgoImg: 'images/heroesCustom/OroroMunroeByJuncal.jpg',
         pack: 'Tormenta by Juncal',
 		url: 'https://drive.google.com/drive/folders/1rtg8Yuxdgf4CeC8zsDpQRscnSKkM718c'
+    },
+	{
+        hero: 'Venom By Picollo',
+        heroImg: 'images/heroesCustom/VenomByPicollo.jpg',
+        alterEgo: "Simbionte",
+        alterEgoImg: 'images/heroesCustom/SimbionteByPicollo.jpg',
+        pack: 'Venom By Picollo',
+		url: 'https://drive.google.com/drive/folders/1lHL6E42NkI0tS5bvNED5sgoKsQxiXffy'
+    },
+	{
+        hero: 'Venompool By CptScorp',
+        heroImg: 'images/heroesCustom/VenompoolByCptScorp.jpg',
+        alterEgo: "Wide Wilson",
+        alterEgoImg: 'images/heroesCustom/WadeWilsonByCptScorp.jpg',
+        pack: 'Venompool By CptScorp',
+		url: 'https://drive.google.com/drive/folders/1dOqWggIRQFjZc-IXl5UYBk1jqxudsu9x'
+    },
+	{
+        hero: 'Visión by DesignHacker',
+        heroImg: 'images/heroesCustom/VisionByDesingHacker.jpg',
+        alterEgo: 'Victor Shade',
+        alterEgoImg: 'images/heroesCustom/VictorShadeByDesingHacker.jpg',
+        pack: 'Visión by DesignHacker',
+		url: 'https://drive.google.com/drive/folders/1Eaq134mJq40tJBO0SUxMxLBB1vIwsJW5'
     },
 	{
         hero: 'X-23 by Caurel-Freakmod',
